6af2a019fa70895ba8a181bb660d47c659f38cc0
nastasiiaaa <nastinsvet@gmail.com>
2024-08-14 22:26:38 +0300

    completed 3 tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,19 @@
-function getElementWidth(content, padding, border) {
+function filterArray(numbers, value) {
 
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
-    const totalWidth = contentWidth + paddingWidth * 2 + borderWidth * 2;
-    return totalWidth;
+   let newArray = [];
+    for (let i = 0; i < numbers.length; i += 1){
+        if (numbers[i]  > value) {
+         newArray.push(numbers[i] ); 
+        }
+    }
+      return newArray;
 }
 
-console . log (getElementWidth( "50px" , "8px" , "4px" )); // 74 
-console . log (getElementWidth( "60px" , "12px" , "8.5px" )); // 101 
-console . log (getElementWidth( "200px" , "0px" , "0px" )); // 200
+
+
+
+console .log(filterArray([ 1 , 2 , 3 , 4 , 5 ], 3 )); // [4, 5] 
+console .log(filterArray([ 1 , 2 , 3 , 4 , 5 ], 4 )); // [5] 
+console .log(filterArray([ 1 , 2 , 3 , 4 , 5 ], 5 )); // [] 
+console .log(filterArray([ 12 , 24 , 8 , 41 , 76 ], 38 )); // [41, 76] 
+console .log(filterArray([ 12 , 24 , 8 , 41 , 76 ], 20 )); // [24, 41, 76]
